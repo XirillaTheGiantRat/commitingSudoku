7912fb4199c18478641fe8aa7172cddbe37db49b
Aliena <alienaruan@gmail.com>
2024-12-14 16:06:49 +0100

    #Test????

--- obj/Debug/net8.0/Sudoku.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sudoku.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sudoku")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78153eb9c3ea7e9e26e2147a7236985d2c24e930")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c1e0c5586f0131faee951c34a470ba53134ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sudoku")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sudoku")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
